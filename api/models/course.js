feac1aa42e66a151f8e5c36e3c3460c9d6434ae6
nmemarcoding <nmemarcoding@gmail.com>
2023-04-06 11:18:12 -0700

    curse scheam created

---
+++ api/models/course.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+
+const courseSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  startDate: {
+    type: Date,
+    required: true,
+  },
+  endDate: {
+    type: Date,
+    required: true,
+  },
+  teacher: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  school: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "School",
+    required: true,
+  },
+  students: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Student",
+    },
+  ],
+});
+
+const Course = mongoose.model("Course", courseSchema);
+
+module.exports = Course;
