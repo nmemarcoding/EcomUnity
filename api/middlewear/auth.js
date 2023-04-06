d4eb7ba106bf79de6667c2e9441632e0d53653f7
nmemarcoding <nmemarcoding@gmail.com>
2023-04-06 10:45:39 -0700

    middlewear updated

--- api/middlewear/auth.js
+++ api/middlewear/auth.js
@@ -21,39 +21,83 @@ function auth(req, res, next) {
 
 // middelewear to check if the user is a admin
 function admin(req, res, next) {
-    if (req.role === "admin") {
-        next();
-    } else {
+    const accessToken = req.body.token;
+    if (!accessToken) {
         return res.status(401).send({ error: 'Unauthorized' });
     }
+    
+    jwt.verify(accessToken,"secret", (err, decoded) => {
+        if (err) {
+            return res.status(401).send({ error: 'Unauthorized' });
+        }
+        if (decoded.role !== "admin") {
+            return res.status(401).send({ error: 'Unauthorized' });
+        }
+        req.userId = decoded.id; // Use the _id claim instead of the userId claim
+        req.role = decoded.role;
+        next();
+    });
 }
 
 // middelewear to check if the user is a teacher
 function teacher(req, res, next) {
-    if (req.role === "teacher") {
-        next();
-    } else {
+    const accessToken = req.body.token;
+    if (!accessToken) {
         return res.status(401).send({ error: 'Unauthorized' });
     }
+    jwt.verify(accessToken,"secret", (err, decoded) => {
+        if (err) {
+            return res.status(401).send({ error: 'Unauthorized' });
+        }
+        if (decoded.role !== "teacher") {
+            return res.status(401).send({ error: 'Unauthorized' });
+        }
+        req.userId = decoded.id; // Use the _id claim instead of the userId claim
+        req.role = decoded.role;
+        next();
+    });
 }
 
 // middelewear to check if the user is a student
 function student(req, res, next) {
-    if (req.role === "student") {
-        next();
-    } else {
+    const accessToken = req.body.token;
+    if (!accessToken) {
         return res.status(401).send({ error: 'Unauthorized' });
     }
+
+    jwt.verify(accessToken,"secret", (err, decoded) => {
+        if (err) {
+            return res.status(401).send({ error: 'Unauthorized' });
+        }
+        if (decoded.role !== "student") {
+            return res.status(401).send({ error: 'Unauthorized' });
+        }
+        req.userId = decoded.id; // Use the _id claim instead of the userId claim
+        req.role = decoded.role;
+        next();
+    });
 }
 
 // middelewear to check if the user is a godAdmin
 function godAdmin(req, res, next) {
-    if (req.role === "godAdmin") {
-        next();
-    } else {
+    const accessToken = req.body.token;
+    if (!accessToken) {
         return res.status(401).send({ error: 'Unauthorized' });
     }
+    jwt.verify(accessToken,"secret", (err, decoded) => {
+        if (err) {
+            return res.status(401).send({ error: 'Unauthorized' });
+        }
+        if (decoded.role !== "godAdmin") {
+            return res.status(401).send({ error: 'Unauthorized' });
+        }
+        req.userId = decoded.id; // Use the _id claim instead of the userId claim
+        req.role = decoded.role;
+        next();
+    });
 }
+
+
 
 
 
