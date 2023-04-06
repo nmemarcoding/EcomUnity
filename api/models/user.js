aa50ef5ee8959f4777a9b9802ef88bea89f7ca9c
nmemarcoding <nmemarcoding@gmail.com>
2023-04-06 10:03:34 -0700

    user schema updated

--- api/models/user.js
+++ api/models/user.js
@@ -5,10 +5,19 @@ const UserSchema = new mongoose.Schema(
     username: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    isAdmin: {
-      type: Boolean,
-      default: false,
+    role: { type: String, required: true },
+    firstName: { type: String },
+    lastName: { type: String },
+    dateOfBirth: { type: Date },
+    phoneNumber: { type: String },
+    address: {
+      street: { type: String },
+      city: { type: String },
+      state: { type: String },
+      zipCode: { type: String },
+      country: { type: String },
     },
+    profilePicture: { type: String },
   },
   { timestamps: true }
 );
