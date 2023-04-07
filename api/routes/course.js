88e7ff67a2b2f9e3dcec7ebf359c02bb6598d9c0
nmemarcoding <nmemarcoding@gmail.com>
2023-04-07 10:31:08 -0700

    rout to add student to the course

--- api/routes/course.js
+++ api/routes/course.js
@@ -35,6 +35,42 @@ router.post("/", teacher, async (req, res) => {
     }
 });
 
+//  rout to add student to course and cheack if student is in the same school
+router.put("/addStudent", student, async (req, res) => {
+    const { courseId } = req.body;
+    try {
+        // Get the student id from the token
+        const studentId = req.userId;
+        // getting student school
+        const studentSchool = req.school;
+        // Check if the course exists
+        const existingCourse = await Course.findOne({ _id: courseId });
+        if (!existingCourse) {
+            return res.status(400).json({ error: "Course does not exist" });
+        }
+        // Check if the student is in the same school as the course
+        if (existingCourse.school != studentSchool) {
+            return res.status(400).json({ error: "Student is not in the same school as the course" });
+        }
+        // Check if the student is already in the course
+        if (existingCourse.students.includes(studentId)) {
+            return res.status(400).json({ error: "Student already in course" });
+        }
+        // Add the student to the course
+        existingCourse.students.push(studentId);
+        await existingCourse.save();
+        res.status(200).json(existingCourse);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+
+
+
+
+    
+
 
 
 
