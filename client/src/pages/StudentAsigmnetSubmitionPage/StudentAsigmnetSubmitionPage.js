87cf936379657772b5f884db1a6a60b307a2d087
nmemarcoding <nmemarcoding@gmail.com>
2023-05-05 09:33:45 -0700

    assignment oreview add to assignmet page

--- client/src/pages/StudentAsigmnetSubmitionPage/StudentAsigmnetSubmitionPage.js
+++ client/src/pages/StudentAsigmnetSubmitionPage/StudentAsigmnetSubmitionPage.js
@@ -5,6 +5,15 @@ import TextEditor from '../../components/TextEditor/TextEditor'
 
 export default function StudentAsigmnetSubmitionPage() {
    const [value, setValue] = useState("")
+   const [Preview, setPreview] = useState(false)
+
+    const PreviewOnClik = () => {
+        if(Preview){
+            setPreview(false)
+        }else{
+            setPreview(true)
+        }
+    }
 
   return (
     <div className="w-screen ">
@@ -17,7 +26,21 @@ export default function StudentAsigmnetSubmitionPage() {
         </div>
         
         <br></br>
-        <div dangerouslySetInnerHTML={{ __html: value }} />
+        
+        <div className="w-full h-20 bg-gray-100 shadow-lg flex items-center justify-center text-center" onClick={PreviewOnClik}>
+            <span className="ml-0 text-2xl">Assignment Preview</span>
+        </div >
+        
+        {/* if preview is true show a div  */}
+        {Preview && <div className="m-20">
+            <div dangerouslySetInnerHTML={{ __html: value }} />
+            <br></br>
+            
+        </div>}
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded m-20">
+                Submit
+        </button>
+
     </div>
   )
 }
