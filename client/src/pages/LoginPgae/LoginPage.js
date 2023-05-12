ef5a75e713288c54cf8b6c41bbb58b7f89885a8f
nmemarcoding <nmemarcoding@gmail.com>
2023-05-12 11:07:29 -0700

    login page conndected to the server

--- client/src/pages/LoginPgae/LoginPage.js
+++ client/src/pages/LoginPgae/LoginPage.js
@@ -1,20 +1,59 @@
-import React from 'react';
+import {useState} from 'react';
+import {publicRequest} from '../../hooks/requestMethods'
+import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import useStore from '../../store';
 
 const LoginPage = () => {
+    const addUserInfo = useStore(sate=>sate.addUserInfo)
+    const [loginInfo, setLoginInfo] = useState({})
+    const navigate = useNavigate()
+
+    const handleChange = (e) => {
+        setLoginInfo({
+            ...loginInfo,
+            [e.target.id]: e.target.value
+        })
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        try {
+            publicRequest().post("auth/login", loginInfo)
+                .then((res) => {
+                    // if status is 200, set userInfo in store
+                    if (res.status === 200) {
+                        addUserInfo(res.data)
+                        navigate("/")
+                    }
+
+                    
+                })
+                .catch((err) => {
+                    console.log(err)
+                })
+        }
+        catch (error) {
+            console.log(error.response.data.message)
+        }
+        
+    }
+
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
       <div className="w-full max-w-md p-4 bg-white rounded-lg shadow-md">
         <h2 className="text-2xl font-bold mb-4">Login</h2>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="mb-4">
             <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-700">
-              Email
+              username
             </label>
             <input
-              type="email"
-              id="email"
+              type="usename"
+              id="username"
               className="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
               placeholder="Enter your email"
+              onChange={handleChange} 
             />
           </div>
           <div className="mb-6">
@@ -26,6 +65,7 @@ const LoginPage = () => {
               id="password"
               className="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
               placeholder="Enter your password"
+              onChange={handleChange}
             />
           </div>
           <div className="flex items-center justify-between">
@@ -35,12 +75,13 @@ const LoginPage = () => {
             >
               Sign In
             </button>
-            <a
-              href="#"
-              className="text-sm text-indigo-500 hover:text-indigo-600 focus:outline-none"
+            <Link
+                to="/signup"
+                className="text-sm text-indigo-500 hover:text-indigo-600 focus:outline-none"
             >
-              Forgot Password?
-            </a>
+                SignUp
+            </Link>
+            
           </div>
         </form>
       </div>
